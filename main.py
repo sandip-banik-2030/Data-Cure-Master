de0dc1ad1d3d711797b95cf06a2557bb40e4f1da
Sandip Banik <sandip.banik2030@gmail.com>
2026-07-23 06:37:41 +0000

    Update main.py sys path

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-from artifacts.datacure.app import app
+import sys
+import os
+
+# Tell Python to include artifacts/datacure in its module search path
+sys.path.insert(0, os.path.abspath("artifacts/datacure"))
+
+# Now import the app from app.py inside artifacts/datacure
+from app import app
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
